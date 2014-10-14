2be327a1b3a8da8cd4e711fe4df1fd212dcbdba9
Kevin <kevin.klapak@gmail.com>
2014-10-14 14:13:20 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImageBox::Application.config.secret_key_base = '21643603f8a2ad256fd5a5d1410eaaa67a2086c78f03adc7aec2b1cc7c050953ce60eece8d3a1d309875f21733d0ab679752be24930c24265afa2dffce8f1afc'
