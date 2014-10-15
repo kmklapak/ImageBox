dc5493d573d2f15f5bd40c06adaf5590e180a089
Kevin <kevin.klapak@gmail.com>
2014-10-14 17:37:57 -0700

    Add tests and CSS

---
+++ spec/requests/images_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Images" do
+  subject { page }
+
+  describe "Index page" do
+    before { visit root_path }
+
+    it { should have_content('Welcome to ImageBox') }
+
+  end
+
+  describe "New page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
+end
