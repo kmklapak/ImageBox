7d2b1621121dc22cea49cf6ffcda98f1c591f05d
Kevin <kevin.klapak@gmail.com>
2014-10-17 01:45:36 -0700

    Add simple forms gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 ImageBox::Application.routes.draw do
-  devise_for :users
+  resources :images
+  devise_for :users, controllers: { registrations: 'registrations'}
   root to: "images#index"
   match '/images',              to: 'images#index',     via: 'get'
   match '/images/:id',          to: 'images#show',      via: 'get'
   match '/images/:id/edit',     to: 'images#edit',      via: 'get'
-  match '/images/new',          to: 'images#new',       via: 'get'
+  match '/images/:id/download', to: 'images#download',  via: 'get', :as => 'image_download'
 end
