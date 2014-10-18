8f3afc78cacd5c1f0ad9180f4621f174fde17421
Kevin <kevin.klapak@gmail.com>
2014-10-17 19:54:09 -0700

    Add validations to models

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,12 @@
 class Image < ActiveRecord::Base
   belongs_to :user
+
+  validates :file, presence: true
+  validates :user, presence: true
+  validates :title, length: {maximum: 100}
+
   mount_uploader :file, ImageUploader
+
+
   self.per_page = 8
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :images
+  validates :username, presence: true
 end
