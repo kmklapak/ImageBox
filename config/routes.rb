ea4a137976e307bef489a3cf1465d0e39c83de09
Kevin <kevin.klapak@gmail.com>
2014-10-21 02:39:39 -0700

    Update routes and initializers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ImageBox::Application.routes.draw do
   resources :images
   devise_for :users, controllers: { registrations: 'registrations'}
   root to: "images#index"
-  match '/images',              to: 'images#index',     via: 'get'
+  match '/images',                              to: 'images#images.js',     via: 'get'
   match '/images/:id',          to: 'images#show',      via: 'get'
   match '/images/:id/edit',     to: 'images#edit',      via: 'get'
   match '/images/:id/download', to: 'images#download',  via: 'get', :as => 'image_download'
