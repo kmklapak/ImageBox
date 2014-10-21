cfd3c3282b32c2a226e7a3fbdde976f962d20e8c
Kevin <kevin.klapak@gmail.com>
2014-10-21 02:42:58 -0700

    Update image model validations

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,8 @@
 class Image < ActiveRecord::Base
+  before_save :default_title
+  include FriendlyId
+  friendly_id :uuid
+
   belongs_to :user
 
   validates :file, presence: true
@@ -7,6 +11,25 @@ class Image < ActiveRecord::Base
 
   mount_uploader :file, ImageUploader
 
-
   self.per_page = 8
+
+
+  def download_url
+    obj = AWS::S3.new.buckets[ENV['S3_BUCKET']].objects[self.file.path]
+
+    obj.url_for( :read,
+                expires_in: 60.minutes,
+                use_ssl: true,
+                response_content_disposition: "attachment; filename=#{self.original_name}" ).to_s
+
+
+  end
+
+  private
+  def default_title
+    if self.title == ""
+      self.title = self.original_name
+    end
+  end
+
 end
